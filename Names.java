2839dbccbe13811ec44e4184ce1030e1a1b0a357
Isaac <jsantosildes@vertere-gs.com>
2026-07-09 09:45:27 +0800

    added Chad

--- Names.java
+++ Names.java
@@ -2,6 +2,7 @@ public class Names {
     public static void main(String[] args) {
         System.out.println("Isaac");
         System.out.println("Emman");
+        System.out.println("Chad");
 
 }
 }
